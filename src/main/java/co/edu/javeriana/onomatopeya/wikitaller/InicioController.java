cc5e9f738e658baff8624be34d68c4667c5265ea
camilaariza <mcamila.ariza25@icloud.com>
2026-08-11 23:43:35 -0500

    Create InicioController.java

---
+++ src/main/java/co/edu/javeriana/onomatopeya/wikitaller/InicioController.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.onomatopeya.wikitaller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "pagina";
+    }
+}
